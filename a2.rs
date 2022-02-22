2ad7cac987c3f7721883c2aeb9d10da77f283403
Deepak <rozankhdka159@gmail.com>
2022-02-22 08:54:46 +0545

    chore: include mathematical opertion

---
+++ a2.rs
@@ -0,0 +1,21 @@
+// Mathematical operation 
+
+// Subtraction which take integer of 32 input and
+// return integer of 32 bit
+fn sub(a: i32, b: i32) -> i32 {
+   a - b;
+}
+
+fn main() {
+    let sum = 2 + 3;
+    let value = 10 - 35;
+    let division = 10 / 2;
+    let mult = 5 * 5;
+
+    let five = sub(8, 3);
+
+    let rem = 6 % 3;
+
+    let rem2 = 6 % 4;
+    
+}
