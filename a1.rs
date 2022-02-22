ee29a4b87d7eb7dc3c5c010e7f1c545cb01c7577
Deepak <rozankhdka159@gmail.com>
2022-02-22 08:49:46 +0545

    chore: include function to print name

---
+++ a1.rs
@@ -0,0 +1,24 @@
+// Topic: Functions
+//
+// Program requirements:
+// *Displays your first and last name
+//
+// Notes: 
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use a println macro to display message to the terminal
+
+// * Use a function to display your first name
+fn first_name() {
+    println!("Deepak");
+}
+
+// * Use a function to display your last name
+fn last_name() {
+    println!("Khadka");
+}
+
+fn main() {
+  first_name();
+  last_name();
+}
